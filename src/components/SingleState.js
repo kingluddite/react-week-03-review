e615a09337cc0559593bb948d3e76b192b25db20
philip howley <howley.phil@gmail.com>
2021-12-27 16:18:03 -0500

    Add single state functional component

---
+++ src/components/SingleState.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function SingleState() {
+  return (
+    <div>
+      <h1>State Name</h1>
+      <p>State Desc</p>
+    </div>
+  );
+}
+
+export default SingleState;
